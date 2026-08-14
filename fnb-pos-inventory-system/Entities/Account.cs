3d8502728937cb5ac0291a792e29e93d747c64e9
NguyenTan33 <122010320+NguyenTan33@users.noreply.github.com>
2026-08-15 01:11:55 +0700

    Dùng DI cho Đăng Nhập
    Xử lý luồng đăng nhập bằng SĐT nếu có rồi báo lỗi

---
+++ fnb-pos-inventory-system/Entities/Account.cs
@@ -0,0 +1,6 @@
+﻿namespace fnb_pos_inventory_system.Entities
+{
+    public class Account
+    {
+    }
+}
---
+++ fnb-pos-inventory-system/Exceptions/BusinessException.cs
@@ -0,0 +1,6 @@
+﻿namespace fnb_pos_inventory_system.Exceptions
+{
+    public class BusinessException
+    {
+    }
+}
